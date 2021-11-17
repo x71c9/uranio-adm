f900e6615243d9aa171c9e01e0f0b9750fd6b23e
Andrea Reni <mail@nbl7.net>
2021-11-17 11:18:04 +0100

    added first UI component. testing inside urn-dot.

---
+++ nuxt/components/UI/Button.ts
@@ -0,0 +1,24 @@
+
+import Vue from 'vue';
+
+type Data = {
+	label:string
+};
+
+type Methods = {
+}
+
+type Computed = {
+}
+
+type Props = {
+}
+
+export default Vue.extend<Data, Methods, Computed, Props>({
+	data():Data {
+		const label = 'Button.';
+		return {
+			label
+		};
+	},
+});
---
+++ nuxt/components/UI/Button.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="button button-1">
+		<div class="text inter-medium-white-20px">
+			<span class="inter-medium-white-20px">{{label}}</span>
+		</div>
+	</div>
+</template>
+<script lang="ts" src="./Button.ts"></script>
+<style type="text/css" src="./Button.css"></style>
