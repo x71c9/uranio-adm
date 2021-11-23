1333cdee21802422bbaef8af9f96968d4fadbb1e
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-11-23 20:16:42 +0100

    working on AllTable Checkboxs.

--- nuxt/components/UI/Checkbox/Checkbox.vue
+++ nuxt/components/UI/Checkbox/Checkbox.vue
@@ -1,8 +1,15 @@
 <template>
-	<div class="checkbox">
+		<!-- :class="{checked: is_checked, disabled: is_disabled, indeterminate: is_indeterminate}" -->
+		<!-- @click="toggle" -->
+	<div
+		class="checkbox"
+		:class="{checked, disabled, indeterminate}"
+		>
 		<img class="state-checked" src="/img/icons/@2x/checkbox-1@2x.png" />
-		<img class="state-disabled" src="/img/icons/@2x/checkbox-1@2x.png" />
+		<img class="state-disabled" src="/img/icons/@2x/checkbox-2@2x.png" />
+		<img class="state-indeterminate" src="/img/icons/@2x/checkbox-3@2x.png" />
 		<input type="checkbox" name="">
+		<!-- <slot></slot> -->
 	</div>
 </template>
 <script lang="ts" src="./Checkbox.ts"></script>
