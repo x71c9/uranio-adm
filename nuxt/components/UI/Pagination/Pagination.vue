376c810418e9667e53b170e4dc67918c52b06993
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-11-24 17:34:43 +0100

    added items_per_page in pagination and limit query page.

--- nuxt/components/UI/Pagination/Pagination.vue
+++ nuxt/components/UI/Pagination/Pagination.vue
@@ -1,123 +1,173 @@
 <template>
 	<div class="pagination inter-normal-chicago-16px">
-		<div class="go-to inter-normal-chicago-16px">
-			<div class="go-to-1">
-				<span class="inter-normal-chicago-16px">Go to page</span>
-			</div>
-			<div class="text-field border-1px-mercury">
-				<div class="left">
-					<!-- :value="(_self.page.index + 1)" -->
-					<input
-						class="pagination_input inter-normal-chicago-16px"
-						type="number"
-						name="go_to_page"
-						min="0"
-						:max="this.page.total_page_num"
-						:placeholder="(_self.page.index + 1)"
-						v-model="change_page_value"
-						@keyup.enter="change_page"
+		<div class="ui-left ui-item-per-page">
+			<div class="ui-button-group">
+				<UIButton
+					:class="{active: this.page.query_limit === 10}"
+					class="secondary"
+					>
+					<nuxt-link
+						:to="`/urn-admin/${atom_name}?page=${this.page.index + 1}&limit=10`"
 						>
-				</div>
+						10
+					</nuxt-link>
+				</UIButton>
+				<UIButton
+					:class="{active: this.page.query_limit === 25}"
+					class="secondary"
+					>
+					<nuxt-link
+						:to="`/urn-admin/${atom_name}?page=${this.page.index + 1}&limit=25`"
+						>
+						25
+					</nuxt-link>
+				</UIButton>
+				<UIButton
+					:class="{active: this.page.query_limit === 50}"
+					class="secondary"
+					>
+					<nuxt-link
+						:to="`/urn-admin/${atom_name}?page=${this.page.index + 1}&limit=50`"
+						>
+						50
+					</nuxt-link>
+				</UIButton>
+				<input
+					class="pagination_input inter-normal-chicago-16px"
+					type="number"
+					name="item_per_page"
+					min="1"
+					:max="128"
+					:placeholder="10"
+					v-model="item_per_page_value"
+					@keyup.enter="change_item_per_page"
+					>
 			</div>
-			<!-- <div class="place"><span class="inter-normal-chicago-16px">page</span></div>-->
+			<div class="pagination-label">
+				<span class="inter-normal-chicago-16px">items per page</span>
+			</div>
 		</div>
-		<div class="ui-page-label">
-			<span class="inter-normal-chicago-16px">Page {{ this.page.index + 1 }} of {{ this.page.total_page_num }}</span>
-		</div>
-		<div class="buttons">
-			<!-- Prev text button -->
-			<nuxt-link
-					v-if="this.page.total_page_num <= 4"
-					:to="`/urn-admin/${atom_name}?page=${this.page.index}`"
-					:event="this.page.index === 0 ? '' : 'click'"
-					class="button-text-arrow button-prev border-1px-mercury"
-					:class="{disabled: this.page.index == 0}"
-					>
-				<img class="icon" src="/img/icons/@2x/icon-228@2x.png">
-				<div class="text-3">Prev</div>
-			</nuxt-link>
-			<div class="default">
-				<!-- Prev arrow button -->
-				<nuxt-link
-					v-if="this.page.total_page_num > 4"
-					:to="`/urn-admin/${atom_name}?page=${this.page.index}`"
-					:event="this.page.index === 0 ? '' : 'click'"
-					class="pag-button button-arrow button-arrow-left border-1px-mercury"
-					:class="{disabled: this.page.index === 0}"
-					>
-					<img class="icon" src="/img/icons/@2x/icon-228@2x.png">
-				</nuxt-link>
-				
-				<!-- button 1 -->
-				<nuxt-link
-					:to="`/urn-admin/${atom_name}?page=1`"
-					class="pag-button"
-					:class="{ active: this.page.index === 0 }"
-					>
-					<div class="text">1</div>
-				</nuxt-link>
-				
-				<div
-					v-if="this.page.index >= 3"
-					class="text-dots inter-medium-mountain-mist-16px"
-					>
-					<span class="inter-medium-mountain-mist-16px">...</span>
+		<div
+			v-if="this.page.total_page_num > 1 && this.page.total_atom_count > 3"
+			class="ui-right"
+			>
+			<div class="go-to inter-normal-chicago-16px">
+				<div class="go-to-1">
+					<span class="inter-normal-chicago-16px">Go to page</span>
 				</div>
-				
-				<nuxt-link
-					v-for="(page, index) in this.page.total_page_num"
-					v-if="index > 0 && index > _self.page.index - 2 && index < _self.page.index + 2 && index < _self.page.total_page_num - 1"
-					:key="index"
-					:to="`/urn-admin/${atom_name}?page=${index + 1}`"
-					class="pag-button"
-					:class="{ active: (index === _self.page.index) }"
-					>
-					<div class="text">
-						{{ page }}
+				<div class="text-field border-1px-mercury">
+					<div class="left">
+						<input
+							class="pagination_input inter-normal-chicago-16px"
+							type="number"
+							name="go_to_page"
+							min="1"
+							:max="this.page.total_page_num"
+							:placeholder="(_self.page.index + 1)"
+							v-model="change_page_value"
+							@keyup.enter="change_page"
+							>
 					</div>
-				</nuxt-link>
-				
-				<div
-					v-if="this.page.index <= this.page.total_page_num - 4"
-					class="text-dots inter-medium-mountain-mist-16px"
-					>
-					<span class="inter-medium-mountain-mist-16px">...</span>
 				</div>
-				
-				<!-- button last -->
+			</div>
+			<div class="text-2">
+				<!-- <span class="inter-normal-chicago-16px">Page {{ this.page.index + 1 }} of {{ this.page.total_page_num }}</span> -->
+				<span class="inter-normal-chicago-16px">of {{ this.page.total_page_num }}</span>
+			</div>
+			<div class="buttons">
+				<!-- Prev text button -->
 				<nuxt-link
-					v-if="this.page.total_page_num > 1"
-					:to="`/urn-admin/${atom_name}?page=${this.page.total_page_num}`"
-					class="pag-button"
-					:class="{ active: this.page.index == this.page.total_page_num - 1 }"
-					>
-					<div class="text">{{ this.page.total_page_num }}</div>
+						v-if="this.page.total_page_num <= 4"
+						:to="`/urn-admin/${atom_name}?page=${this.page.index}&limit=${this.page.query_limit}`"
+						:event="this.page.index === 0 ? '' : 'click'"
+						class="button-text-arrow button-prev border-1px-mercury"
+						:class="{disabled: this.page.index == 0}"
+						>
+					<img class="icon" src="/img/icons/@2x/icon-228@2x.png">
+					<div class="text-3">Prev</div>
 				</nuxt-link>
-				
-				<!-- Next arrow button -->
+				<div class="default">
+					<!-- Prev arrow button -->
+					<nuxt-link
+						v-if="this.page.total_page_num > 4"
+						:to="`/urn-admin/${atom_name}?page=${this.page.index}&limit=${this.page.query_limit}`"
+						:event="this.page.index === 0 ? '' : 'click'"
+						class="pag-button button-arrow button-arrow-left border-1px-mercury"
+						:class="{disabled: this.page.index === 0}"
+						>
+						<img class="icon" src="/img/icons/@2x/icon-228@2x.png">
+					</nuxt-link>
+					
+					<!-- button 1 -->
+					<nuxt-link
+						:to="`/urn-admin/${atom_name}?page=1&limit=${this.page.query_limit}`"
+						class="pag-button"
+						:class="{ active: this.page.index === 0 }"
+						>
+						<div class="text">1</div>
+					</nuxt-link>
+					
+					<div
+						v-if="this.page.index >= 3"
+						class="text-dots inter-medium-mountain-mist-16px"
+						>
+						<span class="inter-medium-mountain-mist-16px">...</span>
+					</div>
+					
+					<nuxt-link
+						v-for="(page, index) in this.page.total_page_num"
+						v-if="index > 0 && index > _self.page.index - 2 && index < _self.page.index + 2 && index < _self.page.total_page_num - 1"
+						:key="index"
+						:to="`/urn-admin/${atom_name}?page=${index + 1}&limit=${_self.page.query_limit}`"
+						class="pag-button"
+						:class="{ active: (index === _self.page.index) }"
+						>
+						<div class="text">
+							{{ page }}
+						</div>
+					</nuxt-link>
+					
+					<div
+						v-if="this.page.index <= this.page.total_page_num - 4"
+						class="text-dots inter-medium-mountain-mist-16px"
+						>
+						<span class="inter-medium-mountain-mist-16px">...</span>
+					</div>
+					
+					<!-- button last -->
+					<nuxt-link
+						v-if="this.page.total_page_num > 1"
+						:to="`/urn-admin/${atom_name}?page=${this.page.total_page_num}&limit=${this.page.query_limit}`"
+						class="pag-button"
+						:class="{ active: this.page.index == this.page.total_page_num - 1 }"
+						>
+						<div class="text">{{ this.page.total_page_num }}</div>
+					</nuxt-link>
+					
+					<!-- Next arrow button -->
+					<nuxt-link
+						v-if="this.page.total_page_num > 4"
+						:to="`/urn-admin/${atom_name}?page=${this.page.index + 2}&limit=${this.page.query_limit}`"
+						:event="this.page.index === this.page.total_page_num - 1 ? '' : 'click'"
+						class="pag-button button-arrow button-arrow-right border-1px-mercury"
+						:class="{disabled: this.page.index == this.page.total_page_num - 1}"
+						>
+						<img class="icon" src="/img/icons/@2x/icon-229@2x.png">
+					</nuxt-link>
+				</div>
+				<!-- Next text button -->
 				<nuxt-link
-					v-if="this.page.total_page_num > 4"
-					:to="`/urn-admin/${atom_name}?page=${this.page.index + 2}`"
-					:event="this.page.index === this.page.total_page_num - 1 ? '' : 'click'"
-					class="pag-button button-arrow button-arrow-right border-1px-mercury"
-					:class="{disabled: this.page.index == this.page.total_page_num - 1}"
-					>
-					<img class="icon" src="/img/icons/@2x/icon-229@2x.png">
+						v-if="this.page.total_page_num <= 4"
+						:to="`/urn-admin/${atom_name}?page=${this.page.index + 2}&limit=${this.page.query_limit}`"
+						:event="this.page.index === this.page.total_page_num - 1 ? '' : 'click'"
+						class="button-text-arrow button-next border-1px-mercury"
+						:class="{disabled: this.page.index == this.page.total_page_num - 1}"
+						>
+					<div class="text-4">Next</div>
+					<img class="icon-1" src="/img/icons/@2x/icon-229@2x.png">
 				</nuxt-link>
 			</div>
-			<!-- Next text button -->
-			<nuxt-link
-					v-if="this.page.total_page_num <= 4"
-					:to="`/urn-admin/${atom_name}?page=${this.page.index + 2}`"
-					:event="this.page.index === this.page.total_page_num - 1 ? '' : 'click'"
-					class="button-text-arrow button-next border-1px-mercury"
-					:class="{disabled: this.page.index == this.page.total_page_num - 1}"
-					>
-				<div class="text-4">Next</div>
-				<img class="icon-1" src="/img/icons/@2x/icon-229@2x.png">
-			</nuxt-link>
 		</div>
-		<!-- <div class="ui-page-label-1"><span class="inter-normal-chicago-16px">Page 1 of 45</span></div>-->
 	</div>
 </template>
 <script lang="ts" src="./Pagination.ts"></script>
