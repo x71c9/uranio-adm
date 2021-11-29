f77d632b709f3a4bea2262d69bf97ba852dcec4f
Andrea Reni <mail@nbl7.net>
2021-11-29 18:23:49 +0100

    added ckeck error on keyup change input.

--- nuxt/components/Property/TEXT.ts
+++ nuxt/components/Property/TEXT.ts
@@ -3,8 +3,6 @@ import Vue from 'vue';
 import uranio from 'uranio';
 
 type Data = {
-	// atom: uranio.types.Atom<uranio.types.AtomName>
-	// prop_name: string
 }
 
 type Methods = {
@@ -20,10 +18,6 @@ type Props = {
 	prop_type: string
 }
 
-// type SimpleAtom = {
-//   [k:string]: any
-// }
-
 export default Vue.extend<Data, Methods, Computed, Props>({
 	inject: [
 		'atom',
@@ -32,12 +26,6 @@ export default Vue.extend<Data, Methods, Computed, Props>({
 		'prop_type'
 	],
 	data():Data {
-		// const atom = that.atom;
-		// const prop_name = that.prop_name;
-		// return {
-		//   atom,
-		//   prop_name
-		// };
 		return {};
 	},
 });
