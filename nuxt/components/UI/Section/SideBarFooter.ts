57eb5f62c57eb33704adec51d3eb87502f188b38
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-12-10 21:33:23 +0100

    added setting atom.

---
+++ nuxt/components/UI/Section/SideBarFooter.ts
@@ -0,0 +1,46 @@
+
+import Vue from 'vue';
+
+import uranio from 'uranio';
+
+import { urn_util } from "urn-lib";
+
+import { Entry } from '../../UI/Section/SideBarBody';
+
+type Data = {
+	items: Entry[]
+};
+
+type Methods = {
+}
+
+type Computed = {
+}
+
+type Props = {
+}
+
+export default Vue.extend<Data, Methods, Computed, Props>({
+	data():Data {
+		const items = [] as Entry[];
+		
+		for (const atom_name of uranio.book.atom.get_names()) {
+			const atom_def = uranio.book.atom.get_definition(atom_name as uranio.types.AtomName);
+			let plural = `${atom_name}s`;
+			if(atom_name !== 'setting'){
+				continue;
+			}
+			if(urn_util.object.has_key(atom_def, 'plural') && atom_def.plural){
+				plural = atom_def.plural;
+			}
+			items.push({
+				label: plural,
+				to: `/urn-admin/${atom_name}`,
+				icon: `/img/icons/png/settings.png`
+			});
+		}
+		return {
+			items
+		};
+	},
+});
