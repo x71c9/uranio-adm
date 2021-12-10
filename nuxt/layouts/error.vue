d342a3f5ba446e31090e0e1c92b8d82f56ed3489
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-12-10 20:31:34 +0100

    started developing 404.

--- nuxt/layouts/error.vue
+++ nuxt/layouts/error.vue
@@ -5,14 +5,28 @@
 		<UISectionSideBar class="main-flex ui-section-sidebar"/>
 		<div class="main-flex ui-main">
 			<UISectionHeader />
-			<Nuxt
-				class="ui-main-nuxt"
+		</div>
+		<div class="ui-main-nuxt error">
+			<Error
+				:message="message"
+				:error="error"
 				/>
 		</div>
-		<UINotification />
+		<UINotification/>
 	</div>
 </template>
 <style type="text/css" src="./urn-admin-reset.css"></style>
 <style type="text/css" src="./urn-admin-global.css"></style>
 <style type="text/css" src="./urn-admin.css"></style>
-<script lang="ts" src="./urn-admin.ts"></script>
+<!-- <script lang="ts" src="./error.ts"></script> -->
+<script lang="ts">
+  export default {
+		data(){
+			return{
+				message: 'An error occured.'
+			}
+		},
+		props: ['error'],
+		layout: 'error' // you can set a custom layout for the error page
+	}
+</script>
