0b797874631e75da9a7a9b34ac54fb871d35329e
Andrea Reni <mail@nbl7.net>
2021-12-10 20:31:34 +0100

    started developing 404.

--- nuxt/layouts/urn-admin.ts
+++ nuxt/layouts/urn-admin.ts
@@ -2,8 +2,6 @@ import Vue from 'vue';
 
 import { urn_log } from 'urn-lib';
 
-// import {Notification} from '../components/UI/Notification/Notification';
-
 urn_log.init(
 	urn_log.LogLevel.FUNCTION_DEBUG,
 	urn_log.LogContext.BROWSER,
@@ -11,14 +9,9 @@ urn_log.init(
 );
 
 type Data = {
-	// notification_active: boolean
-	// notification_type: Notification
-	// notification_message: string
-	// notification_timer?: ReturnType<typeof setTimeout>
 }
 
 type Methods = {
-	// notify: (type: Notification, msg:string) => void
 }
 
 type Computed = {
@@ -31,25 +24,8 @@ type Props = {
 export default Vue.extend<Data, Methods, Computed, Props>({
 	data():Data {
 		return {
-			// notification_active: false,
-			// notification_type: Notification.INFO,
-			// notification_message: '',
-			// notification_timer: undefined
 		};
 	},
 	methods: {
-		// notify(type: Notification, msg: string):void{
-		//   alert(msg);
-		//   this.notification_type = type;
-		//   this.notification_message = msg;
-		//   this.notification_active = true;
-		//   if(this.notification_timer){
-		//     clearTimeout(this.notification_timer);
-		//   }
-		//   this.notification_timer = setTimeout(
-		//     () => this.notification_active = false,
-		//     4000
-		//   );
-		// },
 	}
 });
