5188f65022f1be0f0a610c03b53351a96596e3f1
Andrea Reni <mail@nbl7.net>
2021-12-11 17:38:22 +0100

    started working with dropzone.

---
+++ nuxt/pages/urn-admin/media.ts
@@ -0,0 +1,32 @@
+
+import Vue from 'vue';
+
+import Dropzone from 'dropzone';
+
+type Data = {
+};
+
+type Methods = {
+}
+
+type Computed = {
+}
+
+type Props = {
+}
+
+export default Vue.extend<Data, Methods, Computed, Props>({
+	layout(): string {
+		return "urn-admin";
+	},
+	mounted(){
+		const zonedrop = new Dropzone("form.dropzone");
+		zonedrop.on("addedfile", file => {
+			console.log(`File added: ${file.name}`);
+		});
+	},
+	data():Data{
+		return {
+		};
+	}
+});
---
+++ nuxt/pages/urn-admin/media.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		<form action="/target" class="dropzone"></form>
+	</div>
+</template>
+<script lang="ts" src="./media.ts"></script>
