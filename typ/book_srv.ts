ece2f94b83986dd458e0f281ebeec5ac3a14e305
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-12-21 11:10:58 +0100

    quite some changes in order to make Book.Def.Dock.Routes.Route works better with generics.

--- typ/book_srv.ts
+++ typ/book_srv.ts
@@ -22,12 +22,13 @@ export type Book = {
 
 export namespace Book {
 	
-	export type BasicDefinition =
-		book_cln.Book.BasicDefinition;
+	export type BasicDefinition<A extends urn_trx.types.AtomName> =
+		Omit<urn_trx.types.Book.BasicDefinition<A>, 'properties'> &
+		{ properties: book_cln.Book.Definition.Properties }
 	
 	export type Definition<A extends urn_trx.types.AtomName> =
-		Omit<urn_trx.types.Book.Definition<A>, keyof Book.BasicDefinition> &
-		Book.BasicDefinition;
+		Omit<urn_trx.types.Book.Definition<A>, keyof Book.BasicDefinition<A>> &
+		Book.BasicDefinition<A>;
 	
 	export namespace Definition {
 		
@@ -52,3 +53,40 @@ export namespace Book {
 	}
 	
 }
+
+// export const atom_book:Book = {
+//   customer:{
+//     plural: 'customers',
+//     security:{
+//       type: urn_trx.types.BookSecurityType.UNIFORM,
+//       _w: urn_trx.types.BookPermissionType.PUBLIC
+//     },
+//     properties: {
+//       first_name: {
+//         sortable: false,
+//         type: urn_trx.types.BookPropertyType.TEXT,
+//         label: 'First name'
+//       },
+//       last_name: {
+//         type: urn_trx.types.BookPropertyType.TEXT,
+//         label: 'Last name',
+//         is_title: true
+//       }
+//     },
+//     dock:{
+//       url: '/customers',
+//       routes:{
+//         pippi:{
+//           method: urn_trx.types.RouteMethod.GET,
+//           action: urn_trx.types.AuthAction.READ,
+//           url: '/pippo',
+//           return: Number,
+//           call: async (req:urn_trx.types.Api.Request<'customer', 'pippi'>):Promise<number> => {
+//             console.log(req.route_name);
+//             return 899;
+//           }
+//         }
+//       }
+//     }
+//   },
+// };
