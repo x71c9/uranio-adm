dff6a4426ff5000b50faf0bab00159da5ef010dc
Andrea Reni <mail@nbl7.net>
2022-01-08 12:03:55 +0100

    updated homepage.

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
 	#homepage
 		h1 {{name}}
-		NuxtLink(to='/urn-admin') Admin
 		br
-		NuxtLink(to='/about') About
 		br
-		NuxtLink(to='/contact') Contact
+		NuxtLink(to='/urn-admin' class="button home_go") Go to admin
+		<!-- br -->
+		<!-- NuxtLink(to='/about') About -->
+		<!-- br -->
+		<!-- NuxtLink(to='/contact') Contact -->
 </template>
 <script lang="ts" src="./index.ts"></script>
