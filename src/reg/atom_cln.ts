625f5ea5255b9ec2600f277327923aca31e94e01
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-03-02 19:41:07 +0100

    changed register to atom.

--- src/reg/atom_cln.ts
+++ src/reg/atom_cln.ts
@@ -10,7 +10,7 @@ import * as types from '../client/types';
 
 import {schema} from '../sch/client';
 
-export function register<A extends schema.AtomName>(
+export function atom<A extends schema.AtomName>(
 	atom_definition:types.Book.Definition,
 	atom_name?:A
 ):string{
