2905dfe4da6f2865757fe669d8aace79a01c5073
Andrea Reni <mail@nbl7.net>
2022-03-03 10:48:37 +0100

    removed register atom atomname param.

--- dist/reg/atom_cln.d.ts
+++ dist/reg/atom_cln.d.ts
@@ -4,5 +4,4 @@
  * @packageDocumentation
  */
 import * as types from '../client/types';
-import { schema } from '../sch/client';
-export declare function atom<A extends schema.AtomName>(atom_definition: types.Book.Definition, atom_name?: A): string;
+export declare function atom(atom_definition: types.Book.Definition, atom_name?: string): string;
